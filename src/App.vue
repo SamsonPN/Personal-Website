a90470da0b267617aa290aceb9096dbc0bbceeb2
Samson <samsonn9900@gmail.com>
2020-04-01 22:00:00 -0700

    Removed boilerplate and added own assets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/Landing';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+  html, body {
+    font-size: 100%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  #app {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="landing">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Landing'
+}
+</script>
+
+<style scoped lang="scss">
+    #landing {
+        width: 100%;
+        height: 100vh;
+        background-color: red;
+        color: white;
+        background: {
+            image: url('../assets/landing-image.jpg');
+            size: cover;
+            repeat: no-repeat;
+        }
+    }
+
+
+
+</style>
