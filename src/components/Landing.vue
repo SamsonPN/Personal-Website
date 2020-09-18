aaba65cae0c8b134246f3ce1103746256d52b626
Samson <samsonn9900@gmail.com>
2020-09-17 23:31:20 -0700

    Updated resume link and job title

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,7 +6,7 @@
         </div>
         <div id="infoWrapper">
             <p>Samson Nguyen</p>
-            <p>Full-Stack Developer</p>
+            <p>Software Engineer</p>
             <div id="socialMediaWrapper">
                 <a href="https://github.com/SamsonPN">
                     <img 
@@ -26,7 +26,7 @@
                         alt="gmail icon" 
                         title="Gmail">
                 </a>
-                <a href="https://docs.google.com/document/d/151uofHjC3giaovj-fnXDm9S0r4CU56vfKn9nd88Fv5U/edit?usp=sharing">
+                <a href="https://drive.google.com/file/d/1rCPd6ukeVprvtVqzszGP7pRAXFjkGyMP/view?usp=sharing">
                     <img 
                         src="https://res.cloudinary.com/sotasamson96/image/upload/v1586300978/Personal%20Website/RESUME_kgzpmp.svg" 
                         alt="resume icon" 
